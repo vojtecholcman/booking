929e97e514f94ff70b13f111e575883f57474355
Vojtech Holcman <vojtech.holcman.s@gyarab.cz>
2026-05-27 22:28:43 +0200

    .

--- rooms/apps.py
+++ rooms/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class RoomsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'rooms'
+
+    def ready(self):
+        import pillow_heif
+        pillow_heif.register_heif_opener()
